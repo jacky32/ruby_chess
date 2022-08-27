79659315ae1b7a8db9370e162a88b1c00d5db962
jacky32 <patrki@atlas.cz>
2022-08-27 22:49:28 +0200

    Add generate_possible_moves method

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -4,7 +4,7 @@ require_relative '../piece'
 
 # class for the knight pieces
 class Knight < Piece
-  attr_reader(:piece_color, :visual, :piece_moves, :type)
+  attr_reader(:piece_color, :visual, :piece_moves, :type, :possible_moves, :possible_takes, :id_y, :id_x)
 
   def initialize(piece_color, type, piece_position)
     super
@@ -16,14 +16,39 @@ class Knight < Piece
 
     # return false unless secondary_move_checks_passed?(start_coordinate, end_coordinate)
 
-    start_y = start_coordinate['id_y']
-    start_x = start_coordinate['id_x']
-    end_y = end_coordinate['id_y']
-    end_x = end_coordinate['id_x']
+    # start_y = start_coordinate['id_y']
+    # start_x = start_coordinate['id_x']
+    # end_y = end_coordinate['id_y']
+    # end_x = end_coordinate['id_x']
 
-    return true if valid_vertical?(start_y, start_x, end_y, end_x) || valid_horizontal?(start_y, start_x, end_y, end_x)
+    # return true if valid_vertical?(start_y, start_x, end_y, end_x) || valid_horizontal?(start_y, start_x, end_y, end_x)
+
+    generate_possible_moves
+
+    return true if @possible_moves.include?(end_coordinate['tile'])
 
     false
+  end
+
+  def generate_all
+    @possible_moves = []
+
+    # generates vertical moves
+    @possible_moves << generate_knight_moves(@id_y + 2, @id_y - 2, @id_x + 1, @id_x - 1)
+
+    # generates horizontal moves
+    @possible_moves << generate_knight_moves(@id_y + 1, @id_y - 1, @id_x + 2, @id_x - 2)
+  end
+
+  alias generate_possible_moves generate_all
+  alias generate_possible_takes generate_all
+
+  def generate_knight_moves(tid_y_plus, tid_y_minus, tid_x_plus, tid_x_minus)
+    board = assign_board
+    [board[tid_y_plus][tid_x_plus],
+     board[tid_y_plus][tid_x_minus],
+     board[tid_y_minus][tid_x_plus],
+     board[tid_y_minus][tid_x_minus]]
   end
 
   def valid_vertical?(start_y, start_x, end_y, end_x)
