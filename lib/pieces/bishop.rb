ef103e343454acb80f3205292d20c7d91ce953b6
jacky32 <patrki@atlas.cz>
2022-08-28 15:39:48 +0200

    Fix taking enemy pieces

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -67,7 +67,7 @@ class Bishop < Piece
     generate_possible_takes
     @possible_takes.any? do |take|
       take.id_y == end_coordinate['id_y'] && take.id_x == end_coordinate['id_x']
-      enemy_on_tile?({ 'value' => assign_board[tile.id_y][tile.id_x].content })
+      enemy_on_tile?({ 'value' => assign_board[take.id_y][take.id_x].content })
     end
   end
 end
