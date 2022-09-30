39cc3d4343521c0aab569a02dacd8201540179b7
jacky32 <patrki@atlas.cz>
2022-09-30 21:45:19 +0200

    Add specs for Pawn move method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/game'
+# require_relative '../lib/game'
 
-describe Game do
-end
+# describe Game do
+# end
---
+++ spec/pawn_spec.rb
@@ -0,0 +1,128 @@
+# rubocop:disable all
+
+require_relative '../lib/board'
+require_relative '../lib/pieces/pawn'
+require 'pry-byebug'
+
+
+module PawnSpecHelper
+  def create_coordinate(id_y, id_x)
+    {
+      id_x: id_x,
+      id_y: id_y,
+      tile: @board[id_y][id_x],
+      value: @board[id_y][id_x].content
+    }
+  end
+end
+
+describe Pawn do
+  include PawnSpecHelper
+  describe '#move' do # move movement methods to game to avoid ObjectSpace(board)?
+    context 'white piece' do
+      context 'starting in b2' do
+        before(:each) do
+          @board = Board.new.board
+          @start_id_y = 7
+          @start_id_x = 2
+          @pawn = @board[@start_id_y][@start_id_x].generate_piece('pawn', 'white', { 'id_y': @start_id_y, 'id_x': @start_id_x })
+          @start_coordinate = create_coordinate(@start_id_y, @start_id_x)
+        end
+
+        subject(:pawn) { @pawn }
+
+        it 'moves to b3' do
+          end_id_y = 6
+          end_id_x = 2
+          end_coordinate = create_coordinate(end_id_y, end_id_x)
+
+          pawn.move(@start_coordinate, end_coordinate)
+
+          ending_tile = @board[end_id_y][end_id_x].content
+          starting_tile = @board[@start_id_y][@start_id_x].content
+
+          expect(ending_tile).to be(pawn)
+          expect(starting_tile).not_to be(pawn)
+        end
+
+        it "moves to b4" do
+          binding.pry
+          end_id_y = 5
+          end_id_x = 2
+          end_coordinate = create_coordinate(end_id_y, end_id_x)
+
+          pawn.move(@start_coordinate, end_coordinate)
+
+          ending_tile = @board[end_id_y][end_id_x].content
+          starting_tile = @board[@start_id_y][@start_id_x].content
+
+          expect(ending_tile).to be(pawn)
+          expect(starting_tile).not_to be(pawn)
+        end
+
+        it "doesn't move to b1" do
+          end_id_y = 8
+          end_id_x = 2
+          end_coordinate = create_coordinate(end_id_y, end_id_x)
+
+          pawn.move(@start_coordinate, end_coordinate)
+
+          ending_tile = @board[end_id_y][end_id_x].content
+          starting_tile = @board[@start_id_y][@start_id_x].content
+
+          expect(ending_tile).not_to be(pawn)
+          expect(starting_tile).to be(pawn)
+        end
+
+        it "doesn't move to a2" do
+          end_id_y = 7
+          end_id_x = 1
+          end_coordinate = create_coordinate(end_id_y, end_id_x)
+
+          pawn.move(@start_coordinate, end_coordinate)
+
+          ending_tile = @board[end_id_y][end_id_x].content
+          starting_tile = @board[@start_id_y][@start_id_x].content
+
+          expect(ending_tile).not_to be(pawn)
+          expect(starting_tile).to be(pawn)
+        end
+
+        it "doesn't move to c2" do
+          end_id_y = 7
+          end_id_x = 3
+          end_coordinate = create_coordinate(end_id_y, end_id_x)
+
+          pawn.move(@start_coordinate, end_coordinate)
+
+          ending_tile = @board[end_id_y][end_id_x].content
+          starting_tile = @board[@start_id_y][@start_id_x].content
+
+          expect(ending_tile).not_to be(pawn)
+          expect(starting_tile).to be(pawn)
+        end
+
+        it "moves to b3 but doesn't move to b5 afterwards" do
+          end_id_y = 6
+          end_id_x = 2
+          end_coordinate = create_coordinate(end_id_y, end_id_x)
+
+          pawn.move(@start_coordinate, end_coordinate)
+
+          ending_tile = @board[end_id_y][end_id_x].content
+          starting_tile = @board[@start_id_y][@start_id_x].content
+          expect(ending_tile).to be(pawn)
+          expect(starting_tile).not_to be(pawn)
+
+          final_id_y = 4
+          final_id_x = 2
+          final_coordinate = create_coordinate(final_id_y, final_id_x)
+
+          pawn.move(end_coordinate, final_coordinate)
+          expect(@board[final_id_y][final_id_x].content).not_to be(pawn)
+          expect(@board[end_id_y][end_id_x].content).to be(pawn)
+        end
+      end
+    end
+  end
+end
