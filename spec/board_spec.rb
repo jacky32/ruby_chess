d9e04e108899891136a4ce291e1a782bcf63d838
jacky32 <patrki@atlas.cz>
2022-10-01 17:18:57 +0200

    Add tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,31 @@
 require './lib/board'
 
 describe Board do
+  subject(:board) { described_class.new }
   describe '#populate' do
     it 'populates the board with pieces' do
-      board = Board.new
-
       expect(board[7, 2]).to be_nil
 
+      board.populate
+      expect(board[7, 2]).not_to be_nil
+      expect(board[6, 2]).to be_nil
+    end
+  end
+
+  describe '#show_board' do
+    it 'prints the board' do
+      expect do
+        board.show_board
+      end.to output.to_stdout
+    end
+  end
+
+  describe '#[]' do
+    it 'does access the empty board piece' do
+      expect(board[7, 2]).to be_nil
+    end
+
+    it 'does access the filled board piece' do
       board.populate
       expect(board[7, 2]).not_to be_nil
     end
