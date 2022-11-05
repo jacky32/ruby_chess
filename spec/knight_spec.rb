3f0aed301c51ce5d9a2a0b3f1f81755c2c81843e
jacky32 <4532511+jacky32@users.noreply.github.com>
2022-11-05 23:02:48 +0100

    Add tests for black knights

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -47,4 +47,41 @@ describe Knight do
       end
     end
   end
+  context 'black knight' do
+    context 'starting in b1' do
+      before(:each) do
+        id_y = 8
+        id_x = 2
+        @knight = create_piece(board: board, id_y: id_y, id_x: id_x, type: 'knight', color: 'black')
+      end
+      it 'generates all possible moves and takes' do
+        expect(@knight.possible_moves).to be_empty
+        
+        @knight.generate_all(board: board)
+        expect(@knight.possible_moves).to contain_exactly(board[6, 1], board[6, 3], board[7, 4])
+        expect(@knight.possible_takes).to contain_exactly(board[6, 1], board[6, 3], board[7, 4])
+      end
+    end
+
+    context 'starting in d4' do
+      before(:each) do
+        id_y = 5
+        id_x = 4
+        @knight = create_piece(board: board, id_y: id_y, id_x: id_x, type: 'knight', color: 'black')
+      end
+      it 'generates all possible moves and takes' do
+        expect(@knight.possible_moves).to be_empty
+        
+        @knight.generate_all(board: board)
+        expect(@knight.possible_moves).to contain_exactly(board[3, 3], board[3, 5],
+                                                          board[4, 2], board[4, 6],
+                                                          board[6, 2], board[6, 6],
+                                                          board[7, 3], board[7, 5])
+        expect(@knight.possible_takes).to contain_exactly(board[3, 3], board[3, 5],
+                                                          board[4, 2], board[4, 6],
+                                                          board[6, 2], board[6, 6],
+                                                          board[7, 3], board[7, 5])
+      end
+    end
+  end
 end
